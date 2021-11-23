9f69993e35a33e880981834026e33ee62703be32
demidraksin <greatraksin@icloud.com>
2021-11-23 10:22:47 +0300

    Adding logic operations

---
+++ 09_lesson_2311/00_logic_operations.js
@@ -0,0 +1,9 @@
+let login = prompt('Введи логин: ', '');
+let password = prompt('Введи пароль: ', '');
+
+if (login === 'zodiac' && password === '12345qwerty') {
+    alert('Добро пожаловать!')
+} else {
+    alert('Неправильный логин и/или пароль.')
+}
+
